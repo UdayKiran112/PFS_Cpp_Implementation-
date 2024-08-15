21954cc4449cc16fc0b2765743651bd008ee9a4e
Kavuru Naveen Kumar <naveenkumar_b210560cs@nitc.ac.in>
2024-08-15 14:33:11 +0530

    Refactor Vehicle class constructor and message signing

--- cpp/Vehicle.cpp
+++ cpp/Vehicle.cpp
@@ -17,7 +17,6 @@ Vehicle::Vehicle(octet registrationId, Key vehicleKey, octet signatureKey, octet
 Vehicle::Vehicle(){}
 
 Vehicle::Vehicle(csprng *RNG){
-    //only ta should be initilized with TA constructor
     this->ta = TA(RNG);
 }
 octet Vehicle::getRegistrationId()
@@ -74,13 +73,9 @@ using namespace core;
 using namespace Ed25519;
 void Vehicle::requestVerification() {
     octet signkey, virpubkey;
-
-    // Store the result in a local variable first
     octet publicKey = this->getVehicleKey().getPublicKey();
-
-    // Now you can safely take the address of the local variable
-    this->ta.validateRequest(&(this->registrationId), &publicKey, &signkey, &virpubkey);
-    
+    auto temp = this->registrationId;
+    this->ta.validateRequest(&temp, &publicKey, &signkey, &virpubkey);
     this->setSignatureKey(signkey);
     this->setA(virpubkey);
 }
@@ -95,109 +90,35 @@ static char *StrtoCharstar(string s)
 
 void Vehicle::sendingMessage(core::octet vehiclePrivateKey, core::octet signatureKey, Message message){
     char q[EFS_Ed25519], sig[2 * EFS_Ed25519];
-    string s = message.getMessage();
 
-    // Convert the string message to a char array
-    char *msg = StrtoCharstar(s); // Ensure this allocates memory correctly
-    if (!msg) {
-        cout << "Failed to convert message to char*";
-        return;
-    }
-
-    // Initialize octet structures
     octet D = {sizeof(int), sizeof(int), reinterpret_cast<char*>(&vehiclePrivateKey)};
     octet Q = {sizeof(q), sizeof(q), q};
-    octet M = {static_cast<int>(s.length()), static_cast<int>(s.length()), msg};
+    octet M = message.getMessage();
     octet SIG = {sizeof(sig), sizeof(sig), sig};
 
-    // Sign the message
     bool x = signMessage(false, &D, nullptr, &M, &SIG);
     if(!x) {
         cout << "No Signature Generated";
-        delete[] msg; // Clean up memory if necessary
         return;
     }
 
-    // Output the signature
     cout << "Signature= 0x";
     OCT_output(&SIG);
 
-    // Clean up memory if necessary
-    delete[] msg;
 }
 
-// Irrevelant function code unnecessarily
-
-// void Vehicle::validateMessage(Message message, int signatureKey /*prolly BIG*/, int A /*public key datatype*/, int senderPublicKey)
-// {
-//     // TODO
-
-//     auto timestamp = message.getTimestamp();
-//     if (!Validate_Message(timestamp))
-//     {
-//         cout << "Message has Expired\n";
-//         return;
-//     }
-
-//     octet Q,M, SIG;
-
-//     string s = message.getMessage();
-//     char *msg = StrtoCharstar(s);
-//     if (!msg) {
-//         cout << "Failed to convert message to char*";
-//         return;
-//     }
-//     Q = {};
-//     M = {static_cast<int>(s.length()), static_cast<int>(s.length()), msg};
-//     SIG = {sizeof(signatureKey), sizeof(signatureKey), reinterpret_cast<char*>(&signatureKey)};
-
-//     bool verify = verifyMessage(false, &Q, NULL, &M, &SIG);
-
-//     if(!verify) {
-//         cout << "Message has been Comprimised\n";
-//         delete[] msg;
-//     }
-// }
-
-/**
- * Signs a message using the Ed25519 algorithm.
- *
- * @param ph a boolean indicating whether to include the prehash flag
- * @param privateKey the secret key for signing the message
- * @param context additional context for the message
- * @param message the message to be signed
- * @param signature the resulting signature
- *
- * @return true if the message is successfully signed, false otherwise
- *
- * @throws None
- */
 bool signMessage(bool ph, octet *privateKey, octet *context, octet *message, octet *signature)
 {
     using namespace Ed25519;
     return EDDSA_SIGNATURE(ph, privateKey, context, message, signature);
 }
 
-/**
- * Verifies a message using the Ed25519 algorithm.
- *
- * @param ph a boolean indicating whether to include the prehash flag
- * @param publicKey the public key for verification
- * @param context additional context for the message
- * @param message the message to be verified
- * @param signature the signature to verify the message
- *
- * @return true if the message is successfully verified, false otherwise
- *
- * @throws None
- */
 static bool verifyMessage(bool ph, octet *publicKey, octet *context, octet *message, octet *signature)
 {
-
     return EDDSA_VERIFY(ph, publicKey, context, message, signature);
 }
 
-#define T_replay 1000 // Define T_replay with an appropriate value
+#define T_replay 1000
 
 bool Vehicle::Validate_Message(Ed25519::ECP* GeneratorPoint, octet* signedMessage, Ed25519::ECP* PublicKey, Ed25519::ECP* VehiclePublicKey, Ed25519::ECP* B, Ed25519::ECP* A, chrono::system_clock::time_point timeStamp, octet* Message)
 {
@@ -207,26 +128,21 @@ bool Vehicle::Validate_Message(Ed25519::ECP* GeneratorPoint, octet* signedMessag
         return false;
     }
 
-    // Initialize ECP variables
     ECP LHS, RHS, P, Apoint, Bpoint, PubKey, VehPubKey;
-    // Copy values into other points to avoid issues with pointers
     ECP_copy(&P, GeneratorPoint);
     ECP_copy(&PubKey, PublicKey);
     ECP_copy(&VehPubKey, VehiclePublicKey);
     ECP_copy(&Apoint, A);
     ECP_copy(&Bpoint, B);
 
-    // Compute LHS
     BIG signedMessageHash;
     BIG_fromBytes(signedMessageHash, signedMessage->val);
     ECP_mul(&P, signedMessageHash);
     ECP_copy(&LHS, &P);
 
-    // Compute RHS partially -> GK + PKi
     ECP_copy(&RHS, PublicKey);
     ECP_add(&RHS, VehiclePublicKey);
 
-    // Apoint calculation->A*H(PKi || A)
     ECP_add(VehiclePublicKey, A);
     octet A_hash_octet;
     ECP_toOctet(&A_hash_octet, VehiclePublicKey, true);
@@ -236,7 +152,6 @@ bool Vehicle::Validate_Message(Ed25519::ECP* GeneratorPoint, octet* signedMessag
     BIG_fromBytes(A_hash, A_hash_octet.val);
     ECP_mul(A, A_hash);
 
-    // Bpoint calculation -> B*H(M|| T || B)
     ECP_copy(&Bpoint, B);
     octet result, B_octet, B_hash_octet;
     ECP_toOctet(&B_octet, &Bpoint, true);
@@ -249,11 +164,9 @@ bool Vehicle::Validate_Message(Ed25519::ECP* GeneratorPoint, octet* signedMessag
     BIG_fromBytes(B_hash, B_hash_octet.val);
     ECP_mul(&Bpoint, B_hash);
 
-    // Final RHS calculation
     ECP_add(&RHS, &Bpoint);
     ECP_add(&RHS, A);
 
-    // Compare LHS and RHS
     if (!ECP_equals(&LHS, &RHS)) {
         cout << "Message has been Compromised\n";
         return false;
@@ -261,4 +174,3 @@ bool Vehicle::Validate_Message(Ed25519::ECP* GeneratorPoint, octet* signedMessag
 
     return true;
 }
-
